d74420f86d37752bf4b4dca9dee5017aeffca2e4
binRick <rapidvps@gmail.com>
2015-06-16 13:21:59 -0400

    new

--- Collect_Pids.js
+++ Collect_Pids.js
@@ -6,7 +6,7 @@ var BashCommandCachingLogic = require('./BashCommandCachingLogic'),
 module.exports = function(Server, cb) {
     cb = cb || function() {};
     var Collection = {
-        cmd: "ssh -o StrictHostKeyChecking=false "+Server+" ps -o pid -C zfs| grep PID -v|sort|uniq",
+        cmd: "ssh -o StrictHostKeyChecking=false "+Server+" ps -o pid -C zfs| grep grep -v|grep PID -v|sort|uniq",
     };
     Collection.hash = md5(Collection.cmd);
     Collection.List = BashCommandCachingLogic.SyncRaw(Collection.hash, Collection.cmd) || [];
